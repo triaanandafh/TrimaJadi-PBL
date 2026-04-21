8f71d7b78fccc4357f25beb91b34c03a3a1e333f
KarW010 <kartikostudy@gmail.com>
2026-04-21 14:57:58 +0700

    Onboarding Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'pages/onboarding_page.dart';
 import 'pages/main_screen.dart';
 
 void main() => runApp(const MyApp());
@@ -8,9 +9,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MainScreen(), 
+      home: OnboardingPage(),
     );
   }
 }
